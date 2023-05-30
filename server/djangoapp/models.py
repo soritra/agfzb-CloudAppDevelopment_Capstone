ec49220a8ac926e96b8237f0cb19c95c86081d0c
Ludo <tech@soritra.com>
2023-05-30 21:55:15 +0300

    updated mvt

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,13 +2,25 @@ from django.db import models
 from django.utils.timezone import now
 
 
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
+class CarMake(models.Model):
+  name = models.CharField(null=False, max_length=80, default='car make')
+  description = models.CharField(max_length=1000)
 # - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+  
+  def __str__(self):
+    return "Car {} - {}".format(self.id, self.name)
+    
+
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+# class CarDealer:
+#   pass
+
+
+class CarDealer(models.Model):
+  name = models.CharField(null=False, max_length=100, )
+  
+  def __str__(self):
+    return "Dealer: {}".format(self.name)
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +31,43 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
-
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarModel(models.Model):
+  name = models.CharField(null=False, max_length=100, default='car model')
+  SEDAN, HATCHBACK, MPV, SUV, CROSSOVER, COUPE, CONVERTIBLE = 'sedan', 'hatchback', 'mpv', 'suv', 'crossover', 'coupe', 'convertible'  
+  TYPE_CHOICES = (
+    (COUPE, 'Coupe'),
+    (CONVERTIBLE, 'Convertible'),
+    (CROSSOVER, 'Crossover'),
+    (HATCHBACK, 'Hatchback'),
+    (MPV, 'Multi Purpose Vehicle (MPV)'),
+    (SEDAN, 'Sedan'),
+    (SUV, 'Sports Utility Vehicle (SUV)')
+  )
+  type = models.CharField(
+    null=False,
+    max_length=30,
+    choices=TYPE_CHOICES,
+    default=SEDAN
+  )
+  type = models.CharField(null=False, max_length=100, )
+  year = models.DateField(null=True)
+  make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+  dealer = models.ForeignKey(CarDealer, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return "Car model: {} - {} (by {})".format(self.name, self.make.name, self.dealer.name)
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+# class DealerReview:
+#   pass
+
+class DealerReview(models.Model):
+  content = models.TextField()
+  dealer = models.ForeignKey(CarDealer, on_delete=models.CASCADE)
+  date_created_at = models.DateField(default=now)
+  rating = models.FloatField(default=5.0)
+  
+  def __str__(self):
+    return "Dealer review: {} (by {})".format(self.content, self.dealer.name)
+
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,28 +14,67 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
-
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    return render(request, 'djangoapp/contact.html', context)
 
+  
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return redirect('djangoapp:index')
+    else:
+        return redirect('djangoapp:index')
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
+
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
@@ -47,6 +86,7 @@ def get_dealerships(request):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
